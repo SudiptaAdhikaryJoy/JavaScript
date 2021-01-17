e2cea969de7450131b83c2b00e263b81c2696a7e
itsirajul <shirajulislamparvez@gmail.com>
2021-01-18 02:32:56 +0600

    add boolean

--- GettingStarted/variable.js
+++ GettingStarted/variable.js
@@ -1,9 +1,13 @@
-/*First Line of Variable */
+/**First Line of Variable */
 var bananaPrice = 20;
 console.log(bananaPrice);
-/* TypeOf*/
+/**TypeOf*/
 var seenAfter=21;
 console.log(typeof seenAfter);
-/*String*/
+/**String*/
 var cityName="Dhaka"
 console.log(typeof cityName);
+/**Boolean*/
+var isHot=true;
+var isRich=false;
+console.log(typeof isHot);
