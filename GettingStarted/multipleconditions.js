246c267da69e81c6e26ea7b8a39c4d44138299f7
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 04:38:11 +0600

    nothing

---
+++ GettingStarted/multipleconditions.js
@@ -0,0 +1,8 @@
+var jobPaiso = false;
+var savingAmount=5000;
+if(jobPaiso==true && savingAmount>20000){
+    console.log("Aso Tumar Patri Khuji");
+}
+else{
+    console.log("Biya nai kopal e ");
+}
