9cd78b37f1dcb00f268b5a4705fc09ecf1193a58
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 03:44:16 +0600

    add float and number and decimal fixed

---
+++ GettingStarted/integerfloat.js
@@ -0,0 +1,14 @@
+var number1=25;
+var number2='25.5';
+number2=parseInt(number2);
+//number1=''+number1;
+console.log(number1+number2);
+//parse convert string to number
+
+
+//Float Number
+var number3=0.20;
+var number4=0.10;
+var total=number3+number4;
+total=total.toFixed(2);
+console.log(total);
