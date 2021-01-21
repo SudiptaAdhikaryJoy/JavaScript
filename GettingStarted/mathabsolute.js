538d521e4ec47914e0cc7a5385ef1544868e82d9
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 04:15:40 +0600

    perform little complex progam

--- GettingStarted/mathabsolute.js
+++ GettingStarted/mathabsolute.js
@@ -1,9 +1,9 @@
 //negative to positive
-var number=-5;
+/*var number=-5;
 var abNumber=Math.abs(number);
 console.log(abNumber);
 //round figure
-var newnumber=5.99;
+var newnumber=5.1;
 var newround=Math.round(newnumber);
 console.log(newround);
 //celi--upper
@@ -12,3 +12,7 @@ console.log(newnewround);
 //floor--lower
 var newlowerround=Math.floor(newnumber);
 console.log(newlowerround);
+*/
+var number = Math.random()*100;
+var result = Math.round(number);
+console.log(result);
