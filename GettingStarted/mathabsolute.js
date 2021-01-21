2750891663a324b8540c2f78eef23e5ec2b96687
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 04:08:12 +0600

    absolute round celi floor

---
+++ GettingStarted/mathabsolute.js
@@ -0,0 +1,11 @@
+//negative to positive
+var number=-5;
+var abNumber=Math.abs(number);
+console.log(abNumber);
+//round figure
+var newnumber=5.99;
+var newround=Math.round(newnumber);
+console.log(newround);
+//celi--upper
+var newnewround=Math.ceil(newnumber);
+//floor
---
+++ GettingStarted/mathematicaloperations.js
@@ -0,0 +1,8 @@
+var price1=25;
+var price2=35;
+price2++;//--increase decrease
+console.log(price2);
+var name=' daal ';
+//String Addition
+var result= price1+" "+name;
+console.log(result);
