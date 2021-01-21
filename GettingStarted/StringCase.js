9fcbc46c91820a4a88f29803bb60b58f47b060ac
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 03:09:00 +0600

    string basics

---
+++ GettingStarted/StringCase.js
@@ -0,0 +1,10 @@
+var promise="I Promise i will be millionaire";
+var name='Ebrahim';
+promise.toLowerCase();//lowercase
+name.toUpperCase();
+console.log(promise.toLowerCase());
+console.log(name.toUpperCase());
+//Split
+console.log(promise.split('i'));
+//Index
+console.log(promise.indexOf('be'));
