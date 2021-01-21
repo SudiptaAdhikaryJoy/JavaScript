0bcb18a06ad344ec5e9bd9e729eca1b5bd714203
itsirajul <shirajulislamparvez@gmail.com>
2021-01-22 04:41:32 +0600

    learning multiple conditions

--- GettingStarted/multipleconditions.js
+++ GettingStarted/multipleconditions.js
@@ -1,7 +1,10 @@
-var jobPaiso = false;
-var savingAmount=5000;
+var jobPaiso = true;
+var savingAmount=50000;
 if(jobPaiso==true && savingAmount>20000){
     console.log("Aso Tumar Patri Khuji");
+}
+else if(jobPaiso==true){
+    console.log("Accha Koi din pore dekhbo");
 }
 else{
     console.log("Biya nai kopal e ");
