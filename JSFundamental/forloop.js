28049bc66b6051291dc8b99a1562ffe25a9e47a0
itsirajul <shirajulislamparvez@gmail.com>
2021-01-23 03:20:34 +0600

    use for loop

---
+++ JSFundamental/forloop.js
@@ -0,0 +1,11 @@
+
+/*for(var i = 0;i<=10;i++){
+    console.log(i);
+
+}
+*/
+var nums=[22,33,44,55,66,77,88];
+for(var i=0;i<nums.length;i++){
+    var element=nums[i];
+    console.log(element);
+}
