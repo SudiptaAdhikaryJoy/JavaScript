b8f83bd55ea6130cbb480f90f126e0f28f7bdfe1
itsirajul <shirajulislamparvez@gmail.com>
2021-01-23 03:31:25 +0600

    switch

---
+++ JSFundamental/switchcase.js
@@ -0,0 +1,15 @@
+var num=5
+switch(num){
+    case 1000:
+        console.log("I am 1000");
+        break;
+    case 100:
+        console.log("I am 100");
+        break;
+    case 20:
+    case 10:
+        console.log("I am either 10 or 20");
+        break
+    default:
+        console.log("I don't know who you are? ")
+}
