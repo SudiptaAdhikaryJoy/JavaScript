479936e3106f8f3ee0da77a1f4c0e587184aedfe
itsirajul <shirajulislamparvez@gmail.com>
2021-01-23 02:55:19 +0600

    done array

--- JSFundamental/array.js
+++ JSFundamental/array.js
@@ -1,13 +1,39 @@
-var friendsAge =[21,22,23,24]; //Array
+/*var friendsAge =[21,22,23,24]; //Array
 //Add
 var sonaliAge=friendsAge[2];
 //Replace
 friendsAge[1]=21;
 //See Position
 var position = friendsAge.indexOf(45);
-//Push for adding items
+//Push for adding items in Last
 friendsAge.push(15);
 
 
 //Output
 console.log(friendsAge.length);
+//Delete Item in Last
+friendsAge.pop();
+console.log(friendsAge.length);
+//Add Elements in Beginning
+friendsAge.unshift(29);
+console.log(friendsAge);
+//Remove Element in Beginning
+friendsAge.shift(29);
+console.log(friendsAge);
+*/
+var teaLine=['Balam','Habib','Hridoy','Arifin','Bappa']
+
+/*teaLine.push('Monto');
+console.log(teaLine);
+teaLine.pop()
+console.log(teaLine);
+teaLine.shift();
+console.log(teaLine);
+teaLine.unshift('Ferdous');
+console.log(teaLine);
+*/
+
+//Slice
+var part=teaLine.slice(1,4);
+console.log(part);
+console.log(teaLine);
